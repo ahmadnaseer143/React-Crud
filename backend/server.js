8841825b5e9715da1b46d6bcf7cc2991d9c5d7b9
ahmadnaseer143 <anaseer942@gmail.com>
2025-03-27 11:12:51 -0500

    route folder created

--- backend/server.js
+++ backend/server.js
@@ -1,78 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
-import Product from "./models/product.model.js";
-import mongoose from "mongoose";
+import productRoutes from "./routes/product.route.js";
 dotenv.config();
 const app = express();
 
 app.use(express.json()); // alow us to use json data in the body
 
+app.use("/api/products", productRoutes);
+
 app.get("/", (req, res) => {
   res.send("Server is ready ");
-});
-
-app.get("/api/products", async (req, res) => {
-  try {
-    const products = await Product.find({});
-    res.status(200).json({ status: true, data: products });
-  } catch (error) {
-    console.log("Error in fetching products", error.message);
-    res
-      .status(500)
-      .json({ status: false, message: "Error in fetching products" });
-  }
-});
-
-app.post("/api/products", async (req, res) => {
-  const product = req.body; // user send the data of product in req.body
-  if (!product.name || !product.price || !product.image) {
-    return res
-      .status(400)
-      .json({ status: false, message: "Please provide all fields" });
-  }
-
-  const newProduct = new Product(product);
-  try {
-    await newProduct.save();
-    res.status(201).json({ status: true, data: newProduct });
-  } catch (error) {
-    console.log("Error in creating product", error.message);
-    res.status(500).json({ status: false, message: "Server error" });
-  }
-});
-
-app.put("/api/products/:id", async (req, res) => {
-  const { id } = req.params;
-
-  const product = req.body;
-
-  if (!mongoose.Types.ObjectId.isValid(id)) {
-    res.status(404).json({ status: false, message: "Update Id is Not Valid" });
-  }
-  try {
-    const updatedProduct = await Product.findByIdAndUpdate(id, product, {
-      new: true,
-    });
-    res.status(200).json({ status: true, data: updatedProduct });
-  } catch (error) {
-    console.log("Error in Updating product", error.message);
-    res.status(500).json({ status: false, message: "Server error" });
-  }
-});
-
-app.delete("/api/products/:id", async (req, res) => {
-  const { id } = req.params;
-
-  try {
-    await Product.findByIdAndDelete(id);
-    res.status(200).json({ status: true, message: "Product Deleted" });
-  } catch (error) {
-    console.log("Error in Product deletion:", error.message);
-    res
-      .status(404)
-      .json({ status: false, message: "Product Not Deleted. Error" });
-  }
 });
 
 // console.log(process.env.MONGO_URI);
