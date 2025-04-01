74d68cab34e16b86c87e267e71eee193fa545149
ahmadnaseer143 <anaseer942@gmail.com>
2025-03-31 20:05:34 -0500

    different routes created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,20 @@
-import { Button } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
+import { Routes, Route } from "react-router-dom";
+
+import CreatePage from "./Pages/CreatePage";
+import HomePage from "./Pages/HomePage";
+import Navbar from "./Components/Navbar";
 
 function App() {
   return (
     <>
-      <Button>Hello</Button>
+      <Box minH={"100vh"}>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/create" element={<CreatePage />} />
+        </Routes>
+      </Box>
     </>
   );
 }
